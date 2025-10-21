6fa82b9ce236fd6e97515e41ce1271354a2f05f9
Deborah Mangan <your.email@example.com>
2025-10-21 11:44:17 +1000

    fix: configure tailwind postcss plugin

--- web/postcss.config.mjs
+++ web/postcss.config.mjs
@@ -1,8 +1,8 @@
+import tailwindcss from '@tailwindcss/postcss';
+
 /** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: {
-    tailwindcss: {},
-  },
+  plugins: [tailwindcss()],
 };
 
 export default config;
